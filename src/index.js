3180f06e2b17ca1b07476965718c50247778fc71
Gunish Matta <gunishmatta@gmail.com>
2021-08-29 21:45:40 +0530

    added dispatch method

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import { createStore } from "redux";
 const reducer = (state = 0, action) => {
   console.log("Reducer called");
+
+  if (action.type === "INCREMENT") {
+    return state + 1;
+  } else if (action.type === "DECREMENT") {
+    return state - 1;
+  }
+
   return state;
 };
 const store = createStore(reducer, 0);
 
 store.subscribe(() => {
   console.log("Current State is", store.getState());
+});
+
+store.dispatch({
+  type: "INCREMENT",
+});
+
+store.dispatch({
+  type: "INCREMENT",
 });
